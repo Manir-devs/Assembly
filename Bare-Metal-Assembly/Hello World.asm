c9b3d15cb319fcf004c023e0c60e1d4afe26452c
Manir <maniruz.zaman.primary@gmail.com>
2025-11-19 01:26:00 +0530

    Create Hello World.asm

---
+++ Bare-Metal-Assembly/Hello World.asm
@@ -0,0 +1,16 @@
+org 0x7C00
+start:
+        mov si, message
+loop:
+        lodsb
+        cmp al, 0       ;Check if char is null
+        je kill         ;go to kill function
+        mov ah, 0x0E
+        int 0x10
+        jmp loop
+kill:
+        jmp kill        ;kill the process
+message:
+        db "Hello World", 0
+times 510-($-$$) db 0
+dw 0xAA55
