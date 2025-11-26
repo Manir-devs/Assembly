85e35a3ff6ab6eae9e58d1cbf9f42ba14bf1ce6f
Manir-devs <maniruz.zaman.primary@gmail.com>
2025-11-27 00:21:11 +0530

    updated backspace function (Beta)

--- Bare-Metal-Assembly/InfiniteInput.asm
+++ Bare-Metal-Assembly/InfiniteInput.asm
@@ -16,10 +16,16 @@ start:
 input:
     mov ah, 0
     int 0x16
+    call cursorpos
     cmp al, 0x08
     je backspace
     cmp al, 0x0D
     je newline
+    ret
+cursorpos:
+    mov ah, 0x03
+    mov bh, 0
+    int 0x10            ;DH = row, DL = col
     ret
 clearscreen:
     mov ah,0x06
@@ -39,11 +45,24 @@ printer:
     int 0x10
     ret
 backspace:
+    cmp dl, 0
+    je backspacepro
     mov al, 0x08
     call printer
     mov al, ' '
     call printer
     mov al, 0x08
+    ret
+backspacepro:
+    cmp dh, 0
+    je .done
+    dec dh
+    mov dl, 79
+    mov ah, 0x02
+    mov bh, 0
+    int 0x10
+    ret
+.done:
     ret
 newline:
     mov al, 0x0D
